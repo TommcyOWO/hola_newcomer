870bb3de15a8eed39b3fd941415ac24a1c7cc919
KnowScratcher <yianlee2008@gmail.com>
2024-07-07 18:28:12 +0800

    a lot of things

--- hola_newcomer/src/main/java/org/sakura/hola_newcomer/Hola_newcomer.java
+++ hola_newcomer/src/main/java/org/sakura/hola_newcomer/Hola_newcomer.java
@@ -4,13 +4,15 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.sakura.hola_newcomer.Command.TestCommand;
+import org.sakura.hola_newcomer.EventListeners.NewComerGift;
+import org.sakura.hola_newcomer.global.Global;
 
 public final class Hola_newcomer extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         getCommand("hello").setExecutor(new TestCommand(this));
-        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(new NewComerGift(this), this);
         getServer().getConsoleSender().sendMessage(ChatColor.LIGHT_PURPLE+"HolaNewcomer Plugin is RUNNING");
         //System.out.println();
     }
---
+++ hola_newcomer/src/main/java/org/sakura/hola_newcomer/global/Global.java
@@ -0,0 +1,7 @@
+package org.sakura.hola_newcomer.global;
+
+import org.bukkit.NamespacedKey;
+
+public class Global {
+    public static NamespacedKey key;
+}
